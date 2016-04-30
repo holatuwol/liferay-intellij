552e82487575cf1461033ec583d6d71c322f9163
Minhchau Dang <minhchau.dang@liferay.com>
2016-04-29 19:46:12 -0700

    Recognize plugin details

--- streams0/lib/index.js
+++ streams0/lib/index.js
@@ -5,10 +5,19 @@ var streams5 = require('./streams5');
 var streams8 = require('./streams8');
 var streams9 = require('./streams9');
 
-function createProject(workspaceFolder) {
+var createProjectObjectModels = streams9.createProjectObjectModels;
+var createProjectWorkspace = streams9.createProjectWorkspace;
+var getCoreDetails = streams5.getCoreDetails;
+var getCoreFolders = streams5.getCoreFolders;
+var getModuleDetails = streams4.getModuleDetails;
+var getModuleFolders = streams4.getModuleFolders;
+var getPluginDetails = streams5.getPluginDetails;
+var getPluginFolders = streams5.getPluginFolders;
+
+function createProject(portalSourceFolder, pluginSourceFolders) {
 	var initialCWD = process.cwd();
 
-	process.chdir(workspaceFolder);
+	process.chdir(portalSourceFolder);
 
 	if (!shelljs.test('-d', '.idea')) {
 		fs.mkdirSync('.idea');
@@ -18,26 +27,28 @@ function createProject(workspaceFolder) {
 		fs.mkdirSync('.idea/libraries');
 	}
 
-	var coreFolders = streams5.getCoreFolders();
-	var moduleFolders = streams4.getModuleFolders('modules', 5);
+	var coreFolders = getCoreFolders();
+	var moduleFolders = getModuleFolders('modules', 5);
+	var pluginFolders = getPluginFolders(portalSourceFolder, pluginSourceFolders);
 
-	var coreDetails = coreFolders.map(streams5.getCoreDetails);
-	var moduleDetails = moduleFolders.map(streams4.getModuleDetails);
+	var coreDetails = coreFolders.map(getCoreDetails);
+	var moduleDetails = moduleFolders.map(getModuleDetails);
+	var pluginDetails = pluginFolders.map(getPluginDetails);
 
-	streams9.createProjectWorkspace(coreDetails, moduleDetails);
+	createProjectWorkspace(coreDetails, moduleDetails);
 
 	process.chdir(initialCWD);
 };
 
-function createProjectObjectModels(workspaceFolder) {
+function createProjectObjectModels(portalSourceFolder) {
 	var initialCWD = process.cwd();
 
-	process.chdir(workspaceFolder);
+	process.chdir(portalSourceFolder);
 
-	var moduleFolders = streams4.getModuleFolders('modules', 5);
-	var moduleDetails = moduleFolders.map(streams4.getModuleDetails);
+	var moduleFolders = getModuleFolders('modules', 5);
+	var moduleDetails = moduleFolders.map(getModuleDetails);
 
-	streams9.createProjectObjectModels(moduleDetails);
+	createProjectObjectModels(moduleDetails);
 
 	process.chdir(initialCWD);
 };
