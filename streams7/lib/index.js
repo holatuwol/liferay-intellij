eafeac8e76373c35d6413cdda96d0d77660ea0ef
Minhchau Dang <minhchau.dang@liferay.com>
2016-06-06 16:48:24 -0700

    Update streams7 for repository split

--- streams7/lib/index.js
+++ streams7/lib/index.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var streams2 = require('./streams2');
+var streams3 = require('./streams3');
 var streams4 = require('./streams4');
 var streams5 = require('./streams5');
 var streams7 = require('./streams7');
@@ -7,11 +8,13 @@ var streams7 = require('./streams7');
 var createProjectWorkspace = streams7.createProjectWorkspace;
 var getCoreDetails = streams5.getCoreDetails;
 var getCoreFolders = streams5.getCoreFolders;
+var getFilePath = streams5.getFilePath;
 var getModuleDetails = streams4.getModuleDetails;
-var getModuleFolders = streams4.getModuleFolders;
+var getModuleFolders = streams3.getModuleFolders;
+var getPluginDetails = streams5.getPluginDetails;
 var isDirectory = streams2.isDirectory;
 
-function createProject(portalSourceFolder, pluginSourceFolders) {
+function createProject(portalSourceFolder, otherSourceFolders) {
 	var initialCWD = process.cwd();
 
 	process.chdir(portalSourceFolder);
@@ -21,10 +24,30 @@ function createProject(portalSourceFolder, pluginSourceFolders) {
 	}
 
 	var coreFolders = getCoreFolders();
-	var moduleFolders = getModuleFolders('modules', 5);
+
+	var portalSourceModulesRootPath = getFilePath(portalSourceFolder, 'modules');
+
+	var moduleFolders = getModuleFolders(portalSourceFolder, portalSourceModulesRootPath, true);
+	var pluginFolders = [];
+
+	for (var i = 0; i < otherSourceFolders.length; i++) {
+		var otherSourceFolder = otherSourceFolders[i];
+
+		if (otherSourceFolder.indexOf('modules') != -1) {
+			var newFolders = getModuleFolders(portalSourceFolder, otherSourceFolder);
+
+			moduleFolders = moduleFolders.concat(newFolders);
+		}
+		else {
+			var newFolders = getPluginFolders(portalSourceFolder, otherSourceFolder);
+
+			pluginFolders = pluginFolders.concat(newFolders);
+		}
+	}
 
 	var coreDetails = coreFolders.map(getCoreDetails);
 	var moduleDetails = moduleFolders.map(getModuleDetails);
+	var pluginDetails = pluginFolders.map(getPluginDetails);
 
 	createProjectWorkspace(coreDetails, moduleDetails);
 
